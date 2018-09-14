0fc929958f2b3c663f8dfbb4b333a3b19c3621fc
echo-liang <37156357+echo-liang@users.noreply.github.com>
2018-09-14 11:40:06 -0700

    replace hardcode with params

--- config.inc.php
+++ config.inc.php
@@ -29,10 +29,14 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = '10.128.0.207';
-$cfg['Servers'][$i]['port'] = '3306';
-$cfg['Servers'][$i]['user'] = 'root'; 
-$cfg['Servers'][$i]['password'] = 'EcGKUbxtRoPW2vM7';
+//$cfg['Servers'][$i]['host'] = '10.128.0.207';
+//$cfg['Servers'][$i]['port'] = '3306';
+//$cfg['Servers'][$i]['user'] = 'root'; 
+//$cfg['Servers'][$i]['password'] = 'EcGKUbxtRoPW2vM7';
+$cfg['Servers'][$i]['host'] = getenv('PMA_HOST');
+$cfg['Servers'][$i]['port'] = getenv('PMA_PORT');
+$cfg['Servers'][$i]['user'] = getenv('PMA_USER');
+$cfg['Servers'][$i]['password'] = getenv('PMA_PASSWORD');
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
